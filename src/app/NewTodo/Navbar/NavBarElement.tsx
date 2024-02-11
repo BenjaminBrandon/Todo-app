ab557469d033cd171a604351af1f049ad917ff4f
Benjamin Brandon <benb89.bb@gmail.com>
2024-02-11 14:03:07 -0500

    redesign of dropdown

--- src/app/NewTodo/Navbar/NavBarElement.tsx
+++ src/app/NewTodo/Navbar/NavBarElement.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NavBar.module.scss';
+import SvgMenuComponent from '../icons/verticalMenuDotSvg';
+
 //Maybe make a pinned section
+
 const NavBarElement = () => {
+  const [show, setShow] = useState(true);
+
+  const showMenu = () => {
+    setShow(!show);
+  };
+
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <a>Edit</a>
-        </li>
-        <li>
-          <a>Delete</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <button onClick={() => showMenu()}>
+        <SvgMenuComponent />
+      </button>
+      {show ? (
+        <div className={styles.dropDown}>
+          <a>edit</a>
+          <a>delete</a>
+        </div>
+      ) : null}
+    </div>
   );
 };
 
--- src/app/NewTodo/icons/verticalMenuDotSvg.tsx
+++ src/app/NewTodo/icons/verticalMenuDotSvg.tsx
@@ -3,8 +3,8 @@ import { SVGProps } from 'react';
 const SvgMenuComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
+    width="26"
+    height="26"
     fill="currentColor"
     className="bi bi-three-dots-vertical"
     viewBox="0 0 16 16"
